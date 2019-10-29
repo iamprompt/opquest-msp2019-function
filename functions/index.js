48ff952f68ebc482b593c74d33af5854e1e9bc19
wearedprompt <wearedprompt@gmail.com>
2019-10-29 08:42:36 +0700

    Trig Auth - Random Quest

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,35 @@
-const functions = require('firebase-functions');
+// import cors from 'cors'
+// import express from 'express'
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase)
+
+const db = admin.firestore()
+
+exports.randomQuest = functions.auth.user().onCreate(user => {
+  const uid = user.uid
+
+  randomtoDB(uid)
+
+  return null
+})
+
+async function randomtoDB(uid) {
+  const user = db.collection('users').doc(uid)
+
+  const randomQ1 = Math.floor(Math.random() * 10)
+  const randomQ2 = Math.floor(Math.random() * 10)
+  const randomQ3 = Math.floor(Math.random() * 10)
+  const randomQ4 = Math.floor(Math.random() * 10)
+
+  await user.set({
+    rQ1: randomQ1,
+    rQ2: randomQ2,
+    rQ3: randomQ3,
+    rQ4: randomQ4,
+  })
+
+  console.log("Finish Random Quest for " + uid);
+  
+}
